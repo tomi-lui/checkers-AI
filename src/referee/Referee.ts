d707a100a24237e5222877422a6e4c29ff7368a7
Tomi Lui <64671485+6momo1@users.noreply.github.com>
2022-12-16 13:00:17 -0800

    refactored attack logic

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -52,45 +52,24 @@ export default class Referee {
         boardState: Piece[]
     ): Boolean {
 
-        const pawnDirection = (team === TeamType.BLUE) ? -1 : 1;
-
-        // console.log("Referee cheking the move");
-        // console.log("prev", px, py);
-        // console.log("curr", x, y);
-        // console.log("type", type);
-        // console.log("team", team);
-
         if (this.tileIsOccupied(x, y, boardState)) {
             return false
         }
-
-        if (team === TeamType.RED) {
-
-            // movement logic
-            if (y - py === 1 && Math.abs(px - x) === 1) {
-                return true
-            }
-
-            // attack logic
-            if (y - py === 2 && Math.abs(px - x) === 2) {
-
-                // return false if there is no pawn to attack
-                const xDirection = (x - px > 0) ? 1 : -1;
-                if (!this.tileIsOccupiedByOpponent(px + xDirection, py + 1, boardState, team)) {
-                    return false;
-                }
-
-                return true;
-            }
+        
+        const yDirection = (team === TeamType.RED) ? 1 : -1;
+        // movement logic
+        if (y - py === (1 * yDirection) && Math.abs(px - x) === 1) {
+            return true
         }
-        else { // team BLUE
-            // movement logic 
-            if (
-                py - y === 1 &&
-                Math.abs(px - x) === 1
-            ) {
-                return true
+        // attack logic
+        if (y - py === (2 * yDirection) && Math.abs(px - x) === 2) {
+
+            // return false if there is no pawn to attack
+            if (!this.getAttackedPiece(px,py,x,y,type,team,boardState)) {
+                return false
             }
+
+            return true;
         }
         return false;
     }
