5f35cb6e8c5c5d79c8e94fcf93b51bfeefd7a362
Tomi Lui <64671485+6momo1@users.noreply.github.com>
2022-12-22 11:52:54 -0800

    css update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Board from './components/Board/Board'
 import './App.css'
 import { useGameStats } from './Contexts/GameStatsContext';
-import { TeamType } from './Constants';
 
 function App() {
 
@@ -9,19 +8,6 @@ function App() {
 
   return (
     <div className="app">
-
-      {/* <h1>Checkers: </h1> */}
-      {/* <div className='info-section'>
-        <h3 className='turn-block'>Turn: {(gameStats.turn === TeamType.RED) ?
-          <div className="red-font">RED</div> :
-          <div className='blue-font'>BLUE</div>}
-        </h3>
-        <h3 className="red-font">Red: {gameStats.redAttacks}</h3>
-        <h3 className='blue-font'>Blue: {gameStats.blueAttacks}</h3>
-      </div> */}
-
-      <div className="content">
-      </div>
       <Board />
     </div>
   )
